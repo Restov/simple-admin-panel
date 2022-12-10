299338f1e19f251c64534271b7c5c66eebff0fce
Restov <romankotovv@gmail.com>
2022-12-11 01:36:27 +0700

    Add XSS validation

--- app/views/login_view.php
+++ app/views/login_view.php
@@ -1,7 +1,11 @@
+<link rel="stylesheet" href="./styles/login.css">
 </head>
-<body>
-<form method="post" action="/login/login">
-    <input type="text" name="username" />
-    <input type="password" name="password" />
-    <input type="submit" value="Login" />
-</form>
+<body class="text-center">
+    <form class="form-signin" method="post" action="login/login">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="username" class="sr-only">Username</label>
+      <input type="text" id="username" class="form-control" placeholder="Login" name="username" required="" autofocus="">
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required="">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
--- app/views/template_view.php
+++ app/views/template_view.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
     <?php
     if(isset($_SESSION['error'])) {
         echo "<p class='alert alert-danger'>".$_SESSION['error']."</p>";
         unset($_SESSION['error']);
     }
-
+    if($_SERVER['REQUEST_URI'] != '/login'){
+        echo "<a href='/'>Главная</a><br>";
+    }
     if(isset($_SESSION)){
         if($_SESSION['admin'] == 1){
             echo "<a href='login/logout'>Выйти</a><br><br>";
